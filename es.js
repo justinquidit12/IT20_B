124a0e13d43c9ecb2a21ac85831ee61d04336555
justinquidit12 <justinquidit56@gmail.com>
2025-11-18 08:25:57 +0800

    added function compute grade mi sir

--- es.js
+++ es.js
@@ -1,10 +1,48 @@
-function legalAge(age){
+// function legalAge(age){
 
-    if( age >+ 18){
-        console.log("you are not am adult");
+//     if( age >= 18){
+//         console.log("you are not am adult");
+
+//     }else{
+//         console.log("you are not an adult");
+//     }
+// }
+// legalAge(69);
+
+// function computerGrade(grade){
+
+//     let grade = 76;
+
+//     if(grade > 75 && grade > 80)
+//     {
+//         console.log("kaya")
+//     } else if (grade > 80)
+//     {
+//         console.log("pas");
+
+//     }else{
+//         console.log("bagsak");
+//     }
+// }
+
+function computerGrade(grade){
+
+    if(grade >= 94.5){
+        console.log("1")
+    }else if (grade => 91.5){
+console.log("1.25")
+ }else if (grade => 91.5){
+console.log("1.25")
+ }else if (grade => 89.5){
+console.log("1.5")
+ }else if (grade => 86.5){
+console.log("1.75")
+ }else if (grade => 84.5){
+console.log("2")
 
     }else{
-        console.log("you are not an adult");
+        console.log("Not within the transmutation");
     }
 }
-legalAge(69);
+
+computerGrade(87);
