5b32b763e9662878b7071a220d92f866994630aa
justinquidit12 <justinquidit56@gmail.com>
2025-11-18 08:02:05 +0800

    added function airthmrtic

---
+++ math.js
@@ -0,0 +1,28 @@
+
+// function basicOperation(a, b){
+//     return{
+// console.log("addition" + (a+b));
+// console.log("multi" + (a-b));
+// console.log("division" + (a*b));
+// console.log("subtraction" + (a/b));
+
+
+//     }
+// basicOperation(10, 5);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
